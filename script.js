7dddd45d5c4080809970893fbdb34381df191e69
Amit-Patra-04 <k2amitpatra@gmail.com>
2025-07-04 08:35:52 +0530

    Mobile Menu Nav

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", () => {
   navbar.classList.toggle("scrolled", window.scrollY > 0);
 });
 
+// Mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  menuToggle.innerHTML = mobileMenu.classList.contains("active")
+    ? '<i class="ri-close-line"></i>'
+    : '<i class="ri-menu-line"></i>';
+});
